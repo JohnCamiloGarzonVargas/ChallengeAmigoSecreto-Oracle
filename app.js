2123ba4e983ddfeb36a2bc98d2eb8b9f41812e7f
John Camilo Garzón Vargas <u20222207536@usco.edu.co>
2025-02-08 18:46:22 -0500

    Implementar funcionalidad

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    const nombre = document.getElementById("amigo").value;
+    if (nombre === "") {
+        alert("El nombre no puede estar vacío");
+    } else {
+        //No se puedan repetir los nombres
+        if (amigos.includes(nombre)) {
+            alert("El amigo ya existe");
+        } else {
+        amigos.push(nombre);
+        document.getElementById("amigo").value = "";
+
+         //Transforma el nombre en un elemento de la lista "li" 
+        document.getElementById("listaAmigos").innerHTML = amigos.map(amigo => `<li>${amigo}</li>`).join("");
+    }
+}
+}
+
+
+function sortearAmigo() {
+    if (amigos.length < 2) {
+        alert("No hay amigos para sortear, minimo dos personas para sortear");
+    } else {
+        const amigoSorteado = amigos[Math.floor(Math.random() * amigos.length)];
+        document.getElementById("resultado").innerHTML = `El amigo secreto es: ${amigoSorteado}`;
+        
+    }
+}
